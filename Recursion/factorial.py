269856b4a420004a4c15bb848714b191b77a67fe
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-09 11:36:09 +0200

    factorial calculation using iterative and recursive

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -3,6 +3,12 @@ def factorialwithIterative(n):
     for i in range(n,0,-1):
         f=f*i  
     return f 
+def factorial_recursive(n):
+    if n==1:
+        return 1 
+    return n*factorial_recursive(n-1)
 n=5
-fact=factorialwithIterative(5)
+fact=factorialwithIterative(n)
+fact2=factorial_recursive(n)
+print(fact2)
 print(fact)
