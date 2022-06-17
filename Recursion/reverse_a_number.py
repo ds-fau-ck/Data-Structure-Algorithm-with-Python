8c8d7b9a2ea38923634f3ce309bd7f557f2e73d1
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-17 14:31:01 +0200

    reverse a number using global variable

--- Recursion/reverse_a_number.py
+++ Recursion/reverse_a_number.py
@@ -4,4 +4,15 @@ def reverseANumber(n):
         return 
     print(n%10)
     reverseANumber(n//10)
-reverseANumber(3456)
+op=0
+def reverseNum(n):
+    global op
+    if n<10:
+        op=op*10+n 
+        return 
+    op=op*10+n%10
+    reverseNum(n//10)
+
+
+reverseNum(3456)
+print(op)
